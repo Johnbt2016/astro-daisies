b97e7c1988426095dc7331f7fd9ba2a351972017
John <johnbt@runbox.com>
2022-05-31 11:33:47 -0600

    update

--- read-fits/read-fits.py
+++ read-fits/read-fits.py
@@ -1,17 +1,23 @@
-
 from astropy.io import fits
+import numpy as np
 
 image_file = "/pebble_tmp/models/quiescent-galaxy-colour-diagram/HorizonAGN.fits"
 # image_file = "../data/HorizonAGN.fits"
-info = fits.info(image_file)
-image_data = fits.getdata(image_file, ext=0)
+
+image_data = np.array(fits.getdata(image_file))
 
 def return_info():
-    
     shape = image_data.shape
+    print(shape)
 
-    return info, shape
+    return shape
+
+def return_data(layer=0, idx=-1):
+    data = image_data[layer][idx]
+
+    return data
 
 if __name__ == "__main__":
-    info, shape = return_info()
-    print(shape)
+    data = return_data()
+    shape = return_info()
+
