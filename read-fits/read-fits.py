69cf299e677a9e963f00b5028a65707d927f030c
John <johnbt@runbox.com>
2022-05-31 08:16:18 -0600

    update

---
+++ read-fits/read-fits.py
@@ -0,0 +1,12 @@
+
+from astropy.io import fits
+
+image_file = "/pebble_tmp/models/quiescent-galaxy-colour-diagram/HorizonAGN_LAIGLE-DAVIDZON+2019_SPECTRA_z2-4.fits"
+
+image_data = fits.getdata(image_file, ext=0)
+
+def return_info():
+    info = fits.info(image_file)
+    shape = image_data.shape
+
+    return info, shape
