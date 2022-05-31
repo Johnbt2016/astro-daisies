9e8ddf3af8c4319d7c3429e890ca12d3b99e9a1c
John <johnbt@runbox.com>
2022-05-31 08:27:37 -0600

    update

--- read-fits/read-fits.py
+++ read-fits/read-fits.py
@@ -3,7 +3,7 @@ from astropy.io import fits
 
 image_file = "/pebble_tmp/models/quiescent-galaxy-colour-diagram/HorizonAGN_LAIGLE-DAVIDZON+2019_SPECTRA_z2-4.fits"
 info = fits.info(image_file)
-image_data = fits.getdata(image_file, ext=0)
+image_data = fits.getdata(image_file, ext=1)
 
 def return_info():
     
